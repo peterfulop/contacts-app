364bf080b12ec213c1cf2ae84b9cf183aaf2ec92
peterfulop <mrpeterfulop@gmail.com>
2023-01-19 23:01:04 +0100

    feat(*): added the necessary fonts;

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './fonts/Glysa/Glysa.otf';
+import './fonts/LexendDeca/LexendDeca-VariableFont_wght.ttf';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
